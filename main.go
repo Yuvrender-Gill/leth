ec35117935cfc8b61c01c306b22081d02292b71f
ebinks <elizabethjbinks@gmail.com>
2018-08-24 16:31:18 -0400

    added compile subcommand; now compiles contracts with 'leth compile'

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"flag"
+	"os"
 
 	"github.com/noot/leth/core"
 	"github.com/noot/leth/new"
@@ -11,16 +13,39 @@ import (
 func main() {
 	//client := leth.Dial("http://localhost:8545")
 
-	// var input string
-	// fmt.Println("enter contract to compile (.sol file needs to be in contracts/)")
-	// fmt.Scanln(&input)
-	// path, _ := filepath.Abs("./contracts/" + input + ".sol")
-	// fmt.Println("compiling file", path)
-	contracts, err := core.Compile()
-	if err != nil {
-		log.Fatal(err, ": compilation error")
+	help := flag.Bool("help", false, "print out command-line options")
+
+	flag.Parse() 
+	if *help {
+		fmt.Println("\t\x1b[93mleth help\x1b[0m")
+		fmt.Println("\tleth compile: compile all contracts in contracts/ directory")
+		os.Exit(0)
+	}
+
+	compileCommand := flag.NewFlagSet("compile", flag.ExitOnError)
+
+	// subcommands
+	// ./b
+	if len(os.Args) > 1 {
+		switch os.Args[1]{
+			case "compile":
+				compileCommand.Parse(os.Args[2:])
+			default:
+		}
 	} else {
-		fmt.Println("compilation completed. saving binaries in build/ directory.")
+		os.Exit(0)
+	}
+
+	contracts := []string{}
+	var err error
+	if compileCommand.Parsed() {
+		//contractArgs := compileCommand.Args()
+		contracts, err = core.Compile()
+		if err != nil {
+			log.Fatal(err, ": compilation error")
+		} else {
+			fmt.Println("compilation completed. saving binaries in build/ directory.")
+		}
 	}
 
 	//fmt.Println(contracts)
